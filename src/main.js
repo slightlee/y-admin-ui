bb210749f2fc1f7f76d883addd741d9f7fc35d64
morrow <lmm_work@163.com>
2020-12-13 03:33:40 +0800

    国际化实现

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// import '@/theme/theme-2bb87a/index.css'
 import api from './http'
 import i18n from './i18n'
 
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,5 +1,16 @@
 <template>
-    <h2>Main</h2>
+    <div class="site-wrapper site-page--not-found">
+        <div class="site-content__wrapper">
+            <div class="site-content">
+                <h2 class="not-found-title">系统介绍</h2>
+                <p class="not-found-desc">这是系统介绍页面 ...</p>
+                <p class="not-found-desc">这是系统介绍页面 ...</p>
+                <ThemePicker></ThemePicker>
+                <el-button @click="$router.go(-1)">返回上一页</el-button>
+                <el-button type="primary" class="not-found-btn-gohome" @click="$router.push('/')">进入首页</el-button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
