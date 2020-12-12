15b85e25d2af23a57d86f1cb969a384974187b46
morrow <lmm_work@163.com>
2020-12-13 06:44:23 +0800

    动态换肤

--- src/views/MenuBar/MenuBar.vue
+++ src/views/MenuBar/MenuBar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="menu-bar-container">
         <!-- logo -->
-        <div class="logo" :class="collapse?'menu-bar-collapse-width':'menu-bar-width'">
+        <div class="logo" :style="{'background-color':themeColor}"  :class="collapse?'menu-bar-collapse-width':'menu-bar-width'">
             <img src="@/assets/logo.png"/> <div>{{collapse?'':appName}}</div>
         </div>
         <!-- 导航菜单 -->
@@ -57,7 +57,8 @@ export default {
     computed:{
         ...mapState({
             appName: state=>state.app.appName,
-            collapse: state=>state.app.collapse
+            collapse: state=>state.app.collapse,
+            themeColor: state=>state.app.themeColor,
         })
     }
 };
